2b77aef7ba004e5b142e723d28cc21d6886068d6
Lunjiayyds <142912285+Lunjiayyds@users.noreply.github.com>
2024-04-24 18:27:29 +0800

    module.ts

--- YoutubeClone/Frontend/YoutubeCloneUI/src/app/app.module.ts
+++ YoutubeClone/Frontend/YoutubeCloneUI/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadVideoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
