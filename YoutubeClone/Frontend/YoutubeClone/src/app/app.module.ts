7dbd0fd062314fe37d380751f95be2a1a6d12d0e
vianalin13 <lin.viana13@gmail.com>
2024-04-26 01:08:10 +0800

    input video title and description frontend

--- YoutubeClone/Frontend/YoutubeClone/src/app/app.module.ts
+++ YoutubeClone/Frontend/YoutubeClone/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {UploadVideoComponent} from './upload-video/upload-video.component';
-import { FormsModule } from '@angular/forms';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { MatButtonModule } from "@angular/material/button";
@@ -13,6 +13,10 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatOptionModule } from "@angular/material/core";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -30,11 +34,18 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule, 
+    MatSelectModule,
+    MatOptionModule, 
+    MatInputModule,
+    ReactiveFormsModule
   ],
+  
   providers: [
     provideClientHydration()
   ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
