4803a9cc099118dca46ec08bfa0ad5e1f4762d8c
vianalin13 <lin.viana13@gmail.com>
2024-04-25 20:24:47 +0800

    videoDto editvideo yay

---
+++ YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/dto/VideoDto.java
@@ -0,0 +1,29 @@
+package com.youtubeproject.Youtube.Clone.dto;
+
+import java.util.List;
+
+import com.youtubeproject.Youtube.Clone.Model.Comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+//on the client side
+//request body
+public class VideoDto {
+	private String id;
+	private String title;
+	private String description;
+	private String userId;
+	private Integer likes;
+	private String videoUrl;
+	//private Integer viewCount;
+	private String thumbnailUrl;
+	private List<Comment> commentList;
+	
+
+}
