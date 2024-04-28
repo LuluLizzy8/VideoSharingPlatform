ed782d8c965c1141f72ad07cf9acb4f7d7e6aed6
vianalin13 <lin.viana13@gmail.com>
2024-04-29 01:25:04 +0800

    curly bracket

--- YoutubeClone/Frontend/YoutubeClone/src/app/video.service.ts
+++ YoutubeClone/Frontend/YoutubeClone/src/app/video.service.ts
@@ -36,5 +36,6 @@ export class VideoService {
   getVideo(videoId: string): Observable<VideoDto> {
 	//http call to our backend
 	return this.httpClient.get<VideoDto>("http://localhost:8080/api/videos/" + videoId); //return videoDto
+  }
 	
-  } 
+} 
