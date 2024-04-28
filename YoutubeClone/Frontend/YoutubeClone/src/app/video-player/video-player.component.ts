4cb43570dc0b14cfc566c6e86930bb83ec226ef1
vianalin13 <lin.viana13@gmail.com>
2024-04-29 00:19:58 +0800

    video player interface showing

---
+++ YoutubeClone/Frontend/YoutubeClone/src/app/video-player/video-player.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrl: './video-player.component.css'
+})
+export class VideoPlayerComponent {
+	
+	constructor() {	}
+
+	ngOnIt(): void {
+		
+	}
+}
