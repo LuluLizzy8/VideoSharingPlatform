469cbf74ebf052860f6e6168aa17b1746e803102
vianalin13 <lin.viana13@gmail.com>
2024-04-29 02:15:27 +0800

    VIDEO DISPLAY WORKS

--- YoutubeClone/Frontend/YoutubeClone/src/app/video-player/video-player.component.ts
+++ YoutubeClone/Frontend/YoutubeClone/src/app/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -12,7 +12,7 @@ export class VideoPlayerComponent {
 	videoUrl!: string | ""; //can then bind this to source attribute
 	constructor() {	}
 
-	ngOnIt(): void {
+	ngOnInIt(): void {
 		
 	}
 }
