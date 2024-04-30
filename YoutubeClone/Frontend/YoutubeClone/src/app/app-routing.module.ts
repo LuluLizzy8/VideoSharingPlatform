73f4f8a242566523e339814aa1eb7fa3ab1e91b6
vianalin13 <lin.viana13@gmail.com>
2024-04-30 22:56:08 +0800

    routed links

--- YoutubeClone/Frontend/YoutubeClone/src/app/app-routing.module.ts
+++ YoutubeClone/Frontend/YoutubeClone/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { UploadVideoComponent } from './upload-video/upload-video.component';
 import { SaveVideoDetailsComponent} from './save-video-details/save-video-details.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 import { HomeComponent} from "./home/home.component";
+import { SubscriptionsComponent } from "./subscriptions/subscriptions.component";
+import { HistoryComponent } from "./history/history.component";
+import { LikedVideosComponent } from "./liked-videos/liked-videos.component";
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent, },
 	{ path: 'upload-video', component: UploadVideoComponent, },
 	{ path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent, },
 	{ path: 'video-detail/:videoId', component: VideoDetailComponent, },
+	{ path: 'subscriptions', component: SubscriptionsComponent, },
+	{ path: 'history', component: HistoryComponent, },
+	{ path: 'liked-videos', component: LikedVideosComponent, },
 ];
 
 @NgModule({
