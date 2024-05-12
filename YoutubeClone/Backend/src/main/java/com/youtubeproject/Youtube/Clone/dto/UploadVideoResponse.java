c68f387cb41d80771a891daa6d1f876d5ad60112
LuluLizzy <113492524+LuluLizzy8@users.noreply.github.com>
2024-05-13 02:01:49 +0800

    Update UploadVideoResponse.java

--- YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/dto/UploadVideoResponse.java
+++ YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/dto/UploadVideoResponse.java
@@ -4,10 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Data Transfer Object representing the response returned after a video upload operation.
+ * <p>
+ * This class encapsulates the response data for a video upload, including the video's identifier
+ * and the URL where the uploaded video can be accessed.
+ * </p>
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UploadVideoResponse {
+	/**
+     * The unique id for the uploaded video.
+     */
 	private String videoId;
+	/**
+     * The url for the uploaded video.
+     */
 	private String videoUrl;
 }
