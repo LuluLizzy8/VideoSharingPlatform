f1e91f2b82221c2831848c3050c5c9936c000a9d
vianalin13 <lin.viana13@gmail.com>
2024-05-13 01:53:29 +0800

    video.java javadoc commentary

--- YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Model/Video.java
+++ YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Model/Video.java
@@ -11,32 +11,83 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Represents a video in the YouTube Clone application
+ */
 @Document(value = "Video")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Video {
 	
+	/**
+     * The unique identifier for the video
+     */
 	@Id
 	private String id;
+	
+	/**
+     * The title of the video
+     */
 	private String title;
+	
+	/**
+     * The description of the video
+     */
 	private String description;
+	
+	/**
+     * The user ID of the uploader of the video
+     */
 	private String userId;
+	
+	/**
+     * The username of the uploader of the video
+     */
 	private String userName;
+	
+	/**
+     * The number of likes the video received
+     */
 	private AtomicInteger likes = new AtomicInteger(0);
+	
+	/**
+     * The URL of the video
+     */
 	private String videoUrl;
+	
+	/**
+     * The number of views the video has
+     */
 	private AtomicInteger viewCount  = new AtomicInteger(0);
+	
+	/**
+     * The URL of the video thumbnail
+     */
 	private String thumbnailUrl;
+	
+	/**
+     * A map of user IDs and timestamps in a video
+     */
 	private Map<String, Float> playbackPositions = new HashMap<>();
 	
+	/**
+     * Increments the number of likes for the video.
+     */
 	public void incrementLikes() {
 		likes.incrementAndGet();
 	}
 	
+	/**
+     * Decrements the number of likes for the video.
+     */
 	public void decrementLikes() {
 		likes.decrementAndGet();
 	}
 
+	/**
+     * Increments the view count for the video.
+     */
 	public void incrementViewCount() {
 		viewCount.incrementAndGet();
 	}
