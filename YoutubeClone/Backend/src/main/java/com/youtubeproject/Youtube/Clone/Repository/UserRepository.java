8256dcbae0e41c7decb91bc4d45ca2275b4189a5
vianalin13 <lin.viana13@gmail.com>
2024-05-13 02:18:42 +0800

    repos javadoc

--- YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Repository/UserRepository.java
+++ YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Repository/UserRepository.java
@@ -6,6 +6,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.youtubeproject.Youtube.Clone.Model.User;
 
+/**
+ * Repository interface for managing user data in the YouTube Clone application
+ */
 public interface UserRepository extends MongoRepository<User, String> {
+	
+	/**
+	 * Finds a user by their login token
+	 * @param sub The user login token to search for
+	 * @return An optional containing the user if found, otherwise empty
+	 */
 	Optional<User> findBySub(String sub);
 }
--- YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Repository/VideoRepository.java
+++ YoutubeClone/Backend/src/main/java/com/youtubeproject/Youtube/Clone/Repository/VideoRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.youtubeproject.Youtube.Clone.Model.Video;
 
+/**
+ * Repository interface for managing video data in the YouTube Clone application.
+ */
 public interface VideoRepository extends MongoRepository<Video, String>{
 
 }
